fe729fdc3de0f60677888f4387a2b1115ad53c2d
K-Y-Hoo <bookopen1@gmail.com>
2023-10-18 23:02:30 +0900

    feat: API를 사용하여 포켓몬 카드 컴포넌트 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
+import PokeCard from "./components/PokeCard";
 
 function App() {
   const [pokemons, setPokemons] = useState([]);
@@ -28,7 +29,9 @@ function App() {
       <section className="pt-6 flex flex-col justify-content items-center overflow-auto z-0">
         <div className="flex flex-row flex-wrap gap-[16px] items-center justify-center px-2 max-w-4xl">
           {pokemons.length > 0 ? (
-            pokemons.map(({ url, name }, index) => <div>{name}</div>)
+            pokemons.map(({ url, name }, index) => (
+              <PokeCard key={url} url={url} name={name} />
+            ))
           ) : (
             <h2 className="font-medium text-lg text-slate-900 mb-1">
               포켓몬이 없습니다.
